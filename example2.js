0b1a374909452c46684b04875c798c98c1dadeb5
Balasanthosh A <maarimuthu107@gmail.com>
2023-12-04 18:49:26 +0530

    second commit

--- example2.js
+++ example2.js
@@ -5,3 +5,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
